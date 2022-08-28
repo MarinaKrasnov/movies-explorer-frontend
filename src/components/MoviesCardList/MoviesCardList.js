bdf9beb3c6e817ec48d91235dd4b591084917b26
Marina Krasnov <marina.krasnova@gmail.com>
2022-08-28 12:04:50 +0300

    feat: button more

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,17 +3,31 @@ import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCardList.css";
 
 function MoviesCardList({ movies }) {
+  const showned =
+    window.innerWidth < 437 ? 4 : window.innerWidth < 837 ? 7 : 11;
+  const isMore =
+    window.innerWidth < 437 && movies.length > 5
+      ? true
+      : window.innerWidth < 837 && movies.length > 8
+      ? true
+      : window.innerWidth > 837 && movies.length > 12
+      ? true
+      : false;
   return (
-    <section className="movies-card-list">
-      {movies.map((item) => (
-        <MoviesCard
-          item={item}
-          key={item.movieId}
-          /*           onCardLike={handleCardLike}
-            onCardDelete={handleCardDelete} */
-        />
-      ))}
-    </section>
+    <>
+      <section className="movies-card-list">
+        {movies
+          .map((item) => <MoviesCard item={item} key={item.movieId} />)
+          .slice(0, showned)}
+      </section>
+      <div
+        className={`movies-card-list__container-more ${
+          isMore ? "movies-card-list__container-more_visible" : ""
+        }`}
+      >
+        <p className="movies-card-list__more">Ещё</p>
+      </div>
+    </>
   );
 }
 
